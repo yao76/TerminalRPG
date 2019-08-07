b10dd892a2eee6b74614570242d19c737faf2512
Scot Garcia <garcia_scot_k@yahoo.com>
2019-08-07 15:41:55 -0700

    god  has abandoned us

--- Terminal RPG Game/Program.cs
+++ Terminal RPG Game/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 // Team HiveMind RPG
 // Team Members: Scot 1, Scot 2, Scot 3
@@ -196,25 +197,126 @@ namespace Terminal_RPG_Game
         }
     }
 
+    class Display
+    {
+        public void drawRect(int x, int y, int w, int h, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(x, y);
+            string borderTop = "";
+            for (int i = 0; i < w - 2; i++)
+                borderTop += "═";
+            Console.Write("╔" + borderTop + "╗");
+            for (int i = 1; i <= h - 2; i++)
+            {
+                Console.SetCursorPosition(x, y + i);
+                Console.Write("║");
+                Console.SetCursorPosition(x + w - 1, y + i);
+                Console.Write("║");
+            }
+            Console.SetCursorPosition(x, y + h - 1);
+            Console.Write("╚" + borderTop + "╝");
+        }
+        public void clearRect(int x, int y, int w, int h, string character)
+        {
+            string lineClear = character;
+            for (int i = 1; i < w; i++)
+                lineClear += character;
+
+            Console.SetCursorPosition(0, 0);
+            for (int yc = 0; yc < h; yc++)
+            {
+                Console.SetCursorPosition(x, y + yc);
+                Console.Write(lineClear);
+            }
+        }
+        public void drawText(int x, int y, string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(x, y);
+            Console.Write(text);
+        }
+    }
+
     class Program
     {
         int resourceModifer { get; set; }
         static void Main(string[] args)
         {
-            Fighter Diablo = new Fighter("Diablo", 100, 100, 82, 120, 50, 1);
-            Console.WriteLine(Diablo.getHealth);
-            Fighter Uriel = new Fighter("Uriel", 80, 70, 60, 140, 100, 160);
-            Console.WriteLine(Uriel.getHealth);
-            Diablo.punch(Uriel);
-            Console.WriteLine("Diablo casts fuck you on Uriel");
-            Console.WriteLine(Uriel.getHealth);
-            Item longsword = new Item("Flametongue", "A fucking sword", 30, 35);
-            Uriel.PickUpItem(longsword);
-            Uriel.EquipWeapon(longsword.UseItem(Uriel));
-            Uriel.MeleeAttack(Diablo);
-            Console.WriteLine(Diablo.getHealth);
-            Uriel.Ultimate(Diablo);
-            Console.WriteLine(Diablo.getHealth);
+
+            /* ```````````````````````````````````````````````````````````````````````````````````*/
+            /* ```````````````````````````````````````````````````````````````````````````````````*/
+            /* ```````````````````````````````````````````````````````````````````````````````````*/
+
+            Console.CursorVisible = false;
+            Display display = new Display();
+
+
+            display.drawRect(0, 40, 17, 15, ConsoleColor.White);
+            display.drawRect(0, 40, 200, 15, ConsoleColor.White);
+            display.clearRect(16, 40, 1, 1, "╦");
+            display.clearRect(16, 54, 1, 1, "╩");
+
+            display.drawText(4, 42, "Attack", ConsoleColor.White);
+            display.drawText(4, 44, "Skills", ConsoleColor.White);
+            display.drawText(4, 46, "Inventory", ConsoleColor.White);
+            display.drawText(4, 48, "Run", ConsoleColor.White);
+
+
+            int choice = 0;
+            Dictionary<string, object>[] choices = {
+                new Dictionary<string, object>() {
+                    {"x",4}, {"y",42}, {"text", "Attack"}
+                },
+                new Dictionary<string, object>() {
+                    {"x",4}, {"y", 44}, {"text", "Skills"}
+                },
+                new Dictionary<string, object>() {
+                    {"x",4}, {"y", 46}, {"text", "Inventory"}
+                },
+                new Dictionary<string, object>() {
+                    {"x",4}, {"y", 48}, {"text", "Run"}
+                }
+            };
+
+            ConsoleKeyInfo cki;
+            string gameState = "LeftMenu";
+            while (true)
+            {
+                display.drawText((int)choices[choice]["x"], (int)choices[choice]["y"], choices[choice]["text"].ToString(), ConsoleColor.Green);
+
+                Console.SetCursorPosition(0, 0);
+                while (!Console.KeyAvailable)
+                {
+                    Thread.Sleep(16);
+                }
+                display.clearRect(0, 0, 10, 1, " ");
+
+                cki = System.Console.ReadKey(true);
+
+                if (gameState == "LeftMenu")
+                {
+                    if (cki.Key.ToString() == "DownArrow")
+                    {
+                        display.drawText((int)choices[choice]["x"], (int)choices[choice]["y"], choices[choice]["text"].ToString(), ConsoleColor.White);
+                        choice = (choice + 1) % choices.Length;
+                    }
+                    else if (cki.Key.ToString() == "UpArrow")
+                    {
+                        display.drawText((int)choices[choice]["x"], (int)choices[choice]["y"], choices[choice]["text"].ToString(), ConsoleColor.White);
+                        choice--;
+                        if (choice == -1) choice = choices.Length - 1;
+                    }
+                }
+                else if (gameState == "RightMenu")
+                {
+                    Console.WriteLine("wut");
+                }
+
+                Console.ForegroundColor = ConsoleColor.White;
+
+
+            }
         }
     }
 }
